fd9b5ca3cce6f18303b071387d0c6362dcc89390
Lautaro <lautisiri@gmail.com>
2025-06-01 20:42:48 -0300

    menu principal y consultas (apartado visual) realizado

--- src/main/java/uy/edu/um/Main.java
+++ src/main/java/uy/edu/um/Main.java
@@ -1,6 +1,44 @@
-package um.edu.uy;
+package uy.edu.um;
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        MenuConsultas menuConsultas= new MenuConsultas();
+        Scanner scanner= new Scanner(System.in);
+        short opcion;
+
+        do{
+
+            System.out.println(
+                    "\n╔═══════════════════════╗" +
+                            "\n║ 🌟 MENÚ PRINCIPAL 🌟  " +
+                            "\n╠═══════════════════════╣" +
+                            "\n║  1️⃣  Cargar Datos     " +
+                            "\n║  2️⃣  Menú Consultas      " +
+                            "\n║  3️⃣  Salir                  " +
+                            "\n╚═══════════════════════╝"
+            );
+            opcion = scanner.nextShort();
+
+            switch (opcion){
+                case 1:
+                    //Carga de datos
+                    break;
+                case 2:
+                    menuConsultas.mostrarMenu();
+                    break;
+                case 3:
+                    System.out.println("👉Cerrando programa...");
+                    break;
+                default:
+                    System.out.println("👉Opcion invalida no reconocida");
+                    break;
+            }
+
+        }while(opcion!=3);
+
+
+
     }
 }
